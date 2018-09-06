f8a95b5759c80e63f303a3a5b6de54bcedef696a
Ragaie alfy <Ragaie@ehabs-bc-macbook-pro.local>
2018-09-06 12:23:06 +0200

    Initial Commit

---
+++ ShowCityWheatherApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ShowCityWheatherApp
+//
+//  Created by Ragaie alfy on 9/6/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ShowCityWheatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShowCityWheatherApp
+//
+//  Created by Ragaie alfy on 9/6/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
